54bb0f1ddf29af174be644f942ab66e8b9556539
6Yasuo9 <lenovofox1207@gmail.com>
2024-06-10 19:10:36 +0300

    galery complete(without add button)

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 window.addEventListener('scroll', e =>{
    document.body.style.cssText = `--scrollTop: ${this.scrollY}px` 
 })
+
+document.querySelectorAll('.img-container img').forEach(img =>{
+   img.onclick = () => {
+      document.querySelector('.pop-up').style.display = `block`;
+      document.querySelector('.pop-up img').src = img.getAttribute(`src`);
+   }
+})
+
+document.querySelector('.pop-up span').onclick = () => {
+   document.querySelector('.pop-up').style.display = `none`;
+}
